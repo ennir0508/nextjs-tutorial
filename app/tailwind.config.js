3dcb4639b2de5fcddc6c5639012c696834a58edb
ebikatsu <ennir0508+pg@gmail.com>
2022-11-17 22:08:16 +0900

    Initial commit

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // class属性を含む全てのファイルを指定する必要がある(jsx,htmlなど)
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
